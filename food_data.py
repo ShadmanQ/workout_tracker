8a8759c5063d3ea7124377479e35370c6e90f840
ShadmanQ <shadmanhquazi@gmail.com>
2025-05-12 16:41:43 -0400

    fatsecret parsing usable now

--- food_data.py
+++ food_data.py
@@ -1,8 +1,8 @@
 import fatsecret
 import re
-CLIENT_KEY = 'xxxxxxx'
+CLIENT_KEY = ''
 
-CLIENT_SECRET = 'xxxxxxx'
+CLIENT_SECRET = ''
 
 client = fatsecret.Fatsecret(CLIENT_KEY, CLIENT_SECRET)
 
@@ -10,22 +10,24 @@ def get_nutrition_info(query):
     x = client.foods_search(query)
     return parse_info(x)
 
+def get_numbers(nut_info):
+    num_dict = {}
+    serving, others = nut_info.split('-')
+    num_dict['serving_size'] = serving.strip()
+    others_list = [o.strip() for o in others.split("|")]
+    for i in others_list:
+        name, val = i.split(':')
+        val = val.strip()
+        for j in range(len(val)):
+            if val[j].isalpha():
+                num_dict[name] = float(val[0:j])
+                break
+    return num_dict
+
 def parse_info(info):
-    x = [(f['food_name'], f['food_description']) for f in info[0:10]]
-    ret_list = []
+    ret_dict = {}
+    x = [(f['food_name'], get_numbers(f['food_description'])) for f in info[0:10]]
+    for i in info[0:10]:
+        ret_dict[i['food_name']] = get_numbers(i['food_description'])
 
-    for y in x:
-        fuck = [t.strip() for t in y[1].split('-')[1].split('|')]
-        print("fuck")
-        ret_list.append(fuck)
-    print(ret_list)
-    for item in ret_list:
-        get_numbers(item)
-    return 1
-
-def get_numbers(data_point):
-    re_dict = {}
-    for nutrient in data_point:
-        x = nutrient.split(':')
-        re_dict[x[0].strip()] = float(x[1].strip().replace("kcal","").replace("g",""))
-    print(re_dict)
+    return ret_dict
