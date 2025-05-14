31087800b1dad78c9034be3ca27e23349c1a3405
ShadmanQ <shadmanhquazi@gmail.com>
2025-05-14 16:15:58 -0400

    refactor load/export food data, working state now

--- main.py
+++ main.py
@@ -33,20 +33,20 @@ def load_external(past_workouts,user_routines,food_history):
         print("making a user directory")
         os.mkdir('user_routines')
 
-    if os.path.isdir('food_history'):
-        print("food history found")
-        if (len(os.listdir('food_history'))) > 0:
-                print("you have some user routines saved!")
-                for file in os.listdir('food_history'):
-                    with open('./food_history/'+file,"r",encoding='utf-8') as openfile:
-                        food_history.append(json.load(openfile))
-    else:
-        print("making food history directory")
-        os.mkdir('food_history')
-
     with open("exercise_list.json","r",encoding='utf-8') as openfile:
         input_list = json.load(openfile)
     return input_list
+
+def load_food(food_history):
+    if os.path.isdir('food_history'):
+        print("food history found")
+        if (len(os.listdir('food_history'))) > 0:
+                for file in os.listdir('food_history'):
+                    with open('./food_history/'+file,"r",encoding='utf-8') as openfile:
+                        food_history[file.split("_")[0]]=json.load(openfile)
+    else:
+        print("making food history directory")
+        os.mkdir('food_history')
 
 def main():
     '''
@@ -113,18 +113,25 @@ def add_a_workout(user_routines,input_list,x):
             x.add_exercise(input_list[exercse_name])
 
 def add_a_meal(f_history):
-    print(f_history)
-    food = get_nutrition_info(input("what dd you eat?"))
+    food = get_nutrition_info(input("what did you eat?"))
     print("please confirm your choice")
     for i, f in enumerate(food):
         print(f"{i+1}. {f}")
     choice = int(input("Please enter a number"))
     da_keys = list(food.keys())
-    f_history.append((da_keys[choice-1],food[da_keys[choice-1]]))
-    directory = './food_history/'
-    with open(directory+datetime.datetime.now().strftime("%m-%d-%Y")+'_food'+'.json','w') as f:
-        json.dump(f_history,f)
 
+    stwing = datetime.datetime.today().strftime("%m-%d-%Y")
+    if stwing not in f_history:
+        f_history[stwing] = []
+    f_history[stwing].append(((da_keys[choice-1],food[da_keys[choice-1]])))
+
+def export_food(food_history):
+    print("now exporting!")
+    print(food_history)
+    for i in food_history:
+        with open('./food_history/'+i+"_food.json","w",encoding='utf-8') as f_write:
+            json.dump(food_history[i],f_write)
+        f_write.close()
 
 def create_a_routine():
     name = input("what would you like to call this routine?")
@@ -142,4 +149,4 @@ def fitness_journey():
 
 if __name__ == "__main__":
     main()
-    
+
